ae0eae24d4e909ee9c38da4bade4ddd94d4606f4
Jeroo <SALva18??>
2020-01-27 13:46:46 -0400

    Add project files.

---
+++ SandwichBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns.FluentBuilder
+{
+    public abstract class SandwichBuilder
+    {
+        protected Sandwich _sandwich;
+
+        public Sandwich sandwich
+        {
+            get { return _sandwich; }
+        }
+
+        public abstract void AddBread();
+        public abstract void AddProtein();
+        public abstract void AddCheese();
+        public abstract void AddVeggies();
+        public abstract void AddCondiments();
+    }
+}
